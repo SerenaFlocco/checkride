2dbfda55e5433d9e62f5e92107e94629bfdc8240
SerenaFlocco <serena.flocco2@gmail.com>
2021-12-06 09:27:48 +0100

    pipeline test

--- deployment/ocp/generate_image.sh
+++ deployment/ocp/generate_image.sh
@@ -5,7 +5,7 @@ set -e
 source common.sh
 source env.sh
 
-openshift_registry=$(oc -n openshift-image-registry get routes default-route -o jsonpath='{.spec.host}' --validate=false)
+openshift_registry=$(oc -n openshift-image-registry get routes default-route -o jsonpath='{.spec.host}')
 
 docker login $openshift_registry --username oc --password $(oc whoami -t)
 
